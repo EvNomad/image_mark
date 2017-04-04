1608cb67f984aa016b6e499c0e6115b127abfcc7
anton_magids <anton@nuro.im>
2017-04-04 23:37:45 +0300

    fix typo

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Get all imgage elements on page and populate array of predefined images size
+// Get all image elements on page and populate array of predefined images size
 var images = $("img").map(function(){
 	return (this.naturalWidth > 150 && this.naturalHeight > 150) ? this : null;
 }).get();
