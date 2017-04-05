74213468c563835797b1872114229e7ccf6557c2
anton_magids <anton@nuro.im>
2017-04-05 16:00:40 +0300

    collect image urls and send as message to the background.js

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
 // Get all image elements on page and populate array of predefined images size
 var images = $("img").map(function(){
-	return (this.naturalWidth > 150 && this.naturalHeight > 150) ? this : null;
+	return (this.naturalWidth > 150 && this.naturalHeight > 150) ? this.currentSrc : null;
 }).get();
 
-console.log(images);
+// Send images to the backgroud script.
+// The bookmarks api exposed only in the context of the extension.
+chrome.runtime.sendMessage({imgagesArray: images, ext: "imageMark"}, function(response) {
+    console.log(response.status);
+});
